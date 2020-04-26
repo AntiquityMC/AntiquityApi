b8fb75738a4f2d9999bd8012e9665839dc6851c5
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-04-26 19:47:54 +0300

    Set the default sound group of tiles to NONE

--- src/main/java/antiquity/mixin/tile/TileMixin.java
+++ src/main/java/antiquity/mixin/tile/TileMixin.java
@@ -2,8 +2,22 @@ package antiquity.mixin.tile;
 
 import antiquity.api.tile.ExtendedTile;
 import com.mojang.minecraft.level.tile.Tile;
+import com.mojang.minecraft.level.tile.Tile$SoundGroup;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Tile.class)
 abstract class TileMixin implements ExtendedTile {
+    @SuppressWarnings("ShadowTarget")
+    @Shadow
+    public Tile$SoundGroup soundGroup;
+
+    @Inject(method = "<init>(I)V", at = @At("RETURN"))
+    private void antiquity_onConstruct(int id, CallbackInfo info) {
+        // Set the default sound group to NONE because it's a bit annoying to set currently
+        soundGroup = Tile$SoundGroup.NONE;
+    }
 }
