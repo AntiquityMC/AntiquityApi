134c1f1e64a5cc425bb6323ca93cff23b8f8d5e4
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-04-26 15:26:15 +0300

    Implement DirtyChunkSorter

--- src/main/java/antiquity/mixin/DirtyChunkSorterMixin.java
+++ src/main/java/antiquity/mixin/DirtyChunkSorterMixin.java
@@ -1,16 +1,29 @@
 package antiquity.mixin;
 
+import com.mojang.minecraft.Player;
 import com.mojang.minecraft.level.Chunk;
 import com.mojang.minecraft.level.DirtyChunkSorter;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 
 import java.util.Comparator;
 
 @Mixin(DirtyChunkSorter.class)
 public class DirtyChunkSorterMixin implements Comparator<Chunk> {
+    @Shadow
+    private Player player;
+
     @Override
     public int compare(Chunk first, Chunk second) {
-        // TODO: Implement this based on 13a
-        return 0;
+        boolean firstVisible = first.field_286;
+        boolean secondVisible = second.field_286;
+
+        if (firstVisible && !secondVisible) {
+            return -1;
+        } else if (!firstVisible && secondVisible) {
+            return 1;
+        } else {
+            return Float.compare(first.method_87(player), second.method_87(player));
+        }
     }
 }
