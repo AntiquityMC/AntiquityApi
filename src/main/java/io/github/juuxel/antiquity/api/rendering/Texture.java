94a7c6a3187ad557d050a73cb2e9c2f2cd995746
Juuxel <6596629+Juuxel@users.noreply.github.com>
2020-05-15 19:46:37 +0300

    Add new texture format based on modern Minecraft
    Before: /assets/x/textures/y.png:1
    After: x:y#1

--- src/main/java/io/github/juuxel/antiquity/api/rendering/Texture.java
+++ src/main/java/io/github/juuxel/antiquity/api/rendering/Texture.java
@@ -1,6 +1,7 @@
 package io.github.juuxel.antiquity.api.rendering;
 
 import io.github.juuxel.antiquity.api.tile.ExtendedTile;
+import io.github.juuxel.antiquity.api.util.Identifier;
 
 import java.util.Objects;
 
@@ -12,6 +13,15 @@ public final class Texture {
     public Texture(String path, int index) {
         this.path = Objects.requireNonNull(path, "path");
         this.index = index;
+    }
+
+    public Texture(Identifier id, int index) {
+        this(convertToPath(id), index);
+    }
+
+    private static String convertToPath(Identifier id) {
+        Objects.requireNonNull(id, "id");
+        return id.getNamespace().equals("minecraft") ? ("/" + id.getPath() + ".png") : ("/assets/" + id.getNamespace() + "/textures/" + id.getPath() + ".png");
     }
 
     @Override
@@ -29,15 +39,15 @@ public final class Texture {
     }
 
     public static Texture fromString(String texture) {
-        if (texture.contains(":")) {
-            String[] split = texture.split(":");
+        if (texture.contains("#")) {
+            String[] split = texture.split("#");
             if (split.length != 2) {
-                throw new IllegalArgumentException("Texture '" + texture + "' should contain at most one : character!");
+                throw new IllegalArgumentException("Texture '" + texture + "' should contain at most one # character!");
             }
 
-            return new Texture(split[0], Integer.parseInt(split[1]));
+            return new Texture(new Identifier(split[0]), Integer.parseInt(split[1]));
         } else {
-            return new Texture(texture, 0);
+            return new Texture(new Identifier(texture), 0);
         }
     }
 }
